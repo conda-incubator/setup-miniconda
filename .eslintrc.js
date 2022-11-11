d54378530f9c4cfd2b0e8fff5be6b54dc3fd3fad
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2022-11-11 12:40:46 -0500

    Uopdate lint tules

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,23 +11,16 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: "tsconfig.json"
+    project: [
+      "tsconfig.json",
+      "tsconfig.eslint.json"
+    ]
   },
   plugins: ["@typescript-eslint"],
   rules: {
-    "@typescript-eslint/no-floating-promises": ["error", { ignoreVoid: true }],
-    "@typescript-eslint/no-unused-vars": ["warn", { args: "none" }],
-    "@typescript-eslint/no-use-before-define": "off",
-    "@typescript-eslint/camelcase": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/no-namespace": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/ban-ts-comment": "warn",
-    "@typescript-eslint/no-var-requires": "off",
-    "@typescript-eslint/no-empty-interface": "off",
-    "@typescript-eslint/triple-slash-reference": "warn",
-    "@typescript-eslint/no-inferrable-types": "off",
+    "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/naming-convention": [
       "error",
       {
@@ -39,13 +32,23 @@ module.exports = {
         }
       }
     ],
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-floating-promises": ["error", { ignoreVoid: true }],
+    "@typescript-eslint/no-inferrable-types": "off",
+    "@typescript-eslint/no-namespace": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { args: "none" }],
+    "@typescript-eslint/no-use-before-define": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/triple-slash-reference": "warn",
+    "no-case-declarations": "warn",
+    "no-control-regex": "warn",
     "no-inner-declarations": "off",
     "no-prototype-builtins": "off",
-    "no-control-regex": "warn",
     "no-undef": "warn",
-    "no-case-declarations": "warn",
+    "no-unused-vars": "off",
     "no-useless-escape": "off",
-    "prefer-const": "off",
-    "no-unused-vars": "off"
+    "prefer-const": "off"
   }
 };
