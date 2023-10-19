1a0b32ee38eba7ab7c4acd5b415cca3326ad0b5c
Marco Esters <mesters@anaconda.com>
2023-10-19 07:27:24 -0700

    Fix errors

--- src/installer/download-miniconda.ts
+++ src/installer/download-miniconda.ts
@@ -47,13 +47,14 @@ export async function downloadMiniconda(
   inputs: types.IActionInputs
 ): Promise<string> {
   // Check valid arch
-  let arch: string = constants.MINICONDA_ARCHITECTURES[inputs.architecture.toLowerCase()];
+  let arch: string =
+    constants.MINICONDA_ARCHITECTURES[inputs.architecture.toLowerCase()];
   if (!arch) {
     throw new Error(`Invalid arch "${inputs.architecture}"!`);
   }
   // Backwards compatibility: ARM64 used to map to aarch64
-  if (arch === "arm64" && constants.is_LINUX) {
-    arch = constants.MINICONDA_ARCHITECTURES["aarch64"]
+  if (arch === "arm64" && constants.IS_LINUX) {
+    arch = constants.MINICONDA_ARCHITECTURES["aarch64"];
   }
 
   let extension: string = constants.IS_UNIX ? "sh" : "exe";
