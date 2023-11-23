07d4f2e8ddf3ea91c0aba1b68d22c9ae8b77d6bf
Daniel Bast <2790401+dbast@users.noreply.github.com>
2023-11-23 16:38:22 +0100

    Strip/update dependencies
    Unused/missing dependencies identified via:
    
    ```
    npm install -g depcheck && depcheck
    ```
    
    Updated remaining dependencies via:
    
    ```
    npm install -g npm-check-updates && npm-check-updates -u
    ```
    
    Applied changes via:
    ```
    rm -rf node_modules
    npm install
    npm run format
    npm run build
    ```
    
    The prettier config was loaded in eslint, but no rule applied. depcheck thus reported eslint-plugin-prettier as unused. By not loading the prettier eslint config also eslint-config-prettier could be removed.
    
    Less dependencies = lees breakages / vulnerabilities / updates.

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,7 @@ module.exports = {
   },
   root: true,
   extends: [
-    "eslint:recommended",
-    "prettier"
+    "eslint:recommended"
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
